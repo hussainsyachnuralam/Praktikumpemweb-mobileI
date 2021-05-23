199b5ab244bc3f938ece3e6206d0e9df4e4a57e5
hussainsyachnuralam <husinnuralam07@gmail.com>
2021-05-23 13:41:30 +0700

    Pemrograman Web dan Mobile I

---
+++ MODUL VI/PROGRAM/modul 6/sns/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { 
+  Container, 
+  Header, 
+  Content, 
+  Left,
+  Body,
+  Icon,
+  Text,
+  ListItem,
+  Thumbnail,
+  Input,
+  Item, 
+} from 'native-base';
+
+  let helperArray = require('./userList.json')
+export default class ContentExample extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      allUsers: helperArray,
+      usersFiltered: helperArray,
+    };
+  }
+  
+searchUser(textToSearch){
+  this.setState({
+    usersFiltered: this.state.allUsers.filter(i =>
+      i.name.toLowerCase().includes(textToSearch.toLowerCase()),
+      ),
+  });
+}
+
+searchUser(textToSearch){
+  this.setState({
+    usersFiltered: this.state.allUsers.filter(i =>
+      i.name.includes(textToSearch),
+      ),
+  });
+}
+
+  render() {
+    return (
+      <Container>
+        <Header searchBar rounded>
+          <Item>
+          <Icon name="search"/>
+          <Input placeholder="Search User" 
+          onChangeText={Text=> {
+            this.searchUser(Text);
+            }} />
+          </Item>
+          <Left>
+          </Left>
+          </Header>
+        <Content>
+          {this.state.usersFiltered.map((item, index) => (
+            <ListItem avatar>
+              <Left>
+                <Thumbnail source={{uri: item.image}} />
+              </Left>
+              <Body>
+                <Text>{item.name}</Text>
+                <Text note>{item.address}</Text>
+              </Body>
+            </ListItem>
+          ))}
+          </Content>
+          </Container>
+    );
+  }
+}
